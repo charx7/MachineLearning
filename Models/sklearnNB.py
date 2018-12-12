37c9995bc34b9f63f32691bd2b1323e534970663
Carlos Huerta <carlos.huertaso@udlap.mx>
2018-12-12 21:33:26 +0100

    full implementation of NB with test + training acc missing X-val

--- Models/sklearnNB.py
+++ Models/sklearnNB.py
@@ -1,18 +1,8 @@
 import pandas as pd
-import nltk
 import math
-import multiprocessing as mp
 from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer, TfidfVectorizer
-
-from nltk.tokenize import word_tokenize
-from nltk.stem.porter import *
-from nltk.corpus import stopwords
-from nltk.stem.snowball import SnowballStemmer
-from nltk.tokenize import TweetTokenizer
-from tqdm import tqdm
-from heapq import nlargest
 
 import sys
 # User defined Imports ugly python import syntax >:(
@@ -54,23 +44,41 @@ if __name__ == '__main__':
 
     # Do BoW for freq extraction
     trainingFull = trainingFull["text"][:]
+    # Rerturn the transformer and vectorizer objects
     X_train_transformed, count_vect, tf_transformer = transform_tf(trainingFull)
 
     # shape for debugz
-    print('The shape of the transf dataset is: ',X_train_transformed.shape)
-    print('The shape of the training data is: ', trainingFull.shape)
-    print('The shape of the X_train data is: ', X_train.shape)
-    print('The shape of the training labels is: ', y_train.shape)
-    print('The train labels form are: ',y_train.values)
+    # print('The shape of the transf dataset is: ',X_train_transformed.shape)
+    # print('The shape of the training data is: ', trainingFull.shape)
+    # print('The shape of the X_train data is: ', X_train.shape)
+    # print('The shape of the training labels is: ', y_train.shape)
+    # print('The train labels form are: ',y_train.values)
     # Naive Bayes classifier
+
     naive_bayes_classifier = MultinomialNB().fit(X_train_transformed, y_train.values)
+    # Score method of the nb classifier
+    trainingAcc = naive_bayes_classifier.score(X_train_transformed, y_train.values)
+    print('The accuracy of NB on our training data is: ', trainingAcc)
 
-    # Test output of the clasifier
-    docs_new = ['Get your free stuff', 'oh my god taylor mascaras on sale']
+    # convert to array of text values
+    test_tweets = X_test.values
 
+    # Perform vector transformation on the test set
+    test_tweets_counts = count_vect.transform(test_tweets)
+    test_tweets_tfidf = tf_transformer.transform(test_tweets_counts)
+    # Run the score function on the transformed test set
+    testAcc = naive_bayes_classifier.score(test_tweets_tfidf, y_test.values)
+    print('The accuracy of NB on our test set data is: ', testAcc)
+
+    # Test output of the clasifier with fun phrases to be replaced by real tweets
+    docs_new = ['Get your free stuff',
+                'oh my god taylor mascaras on sale',
+                'free indian job get recruited',
+                'im slowly dying while doing this project help']
+    # Transform the text we are going to test
     X_new_counts = count_vect.transform(docs_new)
     X_new_tfidf = tf_transformer.transform(X_new_counts)
-
+    # Call the predict function of the classifier
     predicted = naive_bayes_classifier.predict(X_new_tfidf)
 
     target_names = ['Human', 'Bot']
