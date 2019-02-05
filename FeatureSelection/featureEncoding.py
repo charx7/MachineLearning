2aa733ced600f0ea8ac44777c9358e84f2757029
yfchenShirley <641751045@qq.com>
2019-02-05 17:24:29 +0100

    Feature Encoding and Selection

--- FeatureSelection/featureEncoding.py
+++ FeatureSelection/featureEncoding.py
@@ -4,33 +4,34 @@
 import pandas as pd
 
 def featureEncoding(df):
-	####################################Drop Useless columns##############################################
+	#df = dataOrigin#############################################################################################
 	dataOrigin = df #Origin data
 	# remove useless features manually
-	df = df.drop(['bot', 'id','user_id', 'text', 'source', 'in_reply_to_status_id', 
-	              'in_reply_to_user_id', 'retweeted_status_id'], axis=1)
+	df = df.drop(['bot', 'id','user_id', 'text', 'source','reply_count'], axis=1)
 
-####################################Split continuous data##############################################
-# preprocess feature: favorite_count
-	df['favorite_count'] = pd.cut(df['favorite_count'], bins=[-1, 20, 40, 60, 80, 100], 
-		labels=['favorite_count0_20', 'favorite_count20_40', 'favorite_count40_60', 'favorite_count60_80', 'favorite_count80_100'])
-	# preprocess feature: retweet_count
-	df['retweet_count'] = pd.cut(df['retweet_count'], bins=[-1, 20000, 40000, 60000, 300000], 
-		labels=['retweet_count0_20k', 'retweet_count20_40k', 'retweet_count40_60k', 'retweet_count60_300k'])
+	#truncated
+	df.truncated.loc[~df.truncated.isnull()] = 'Notnan'  # not nan
+	#df.truncated.loc[df.truncated.isnull()] = 0   # nan
+	#possibly_sensitive
+	df.possibly_sensitive.loc[~df.possibly_sensitive.isnull()] = 'Notnan'  # not nan
+	#df.possibly_sensitive.loc[df.possibly_sensitive.isnull()] = 0   # nan
 
-####################################Create dummy variables############################################
-	cat_vars=['place','num_hashtags','num_urls','num_mentions', 'favorite_count', 'retweet_count']
+	####################################Create dummy variables############################################
+	cat_vars=['place','truncated','possibly_sensitive','geo','contributors','favorited','retweeted']
 	for var in cat_vars:
 	    cat_list='var'+'_'+var
 	    cat_list = pd.get_dummies(df[var], prefix=var,prefix_sep='_',dummy_na=True)
 	    data1=df.join(cat_list)
 	    df=data1
 
-	cat_vars=['place','num_hashtags','num_urls','num_mentions', 'favorite_count', 'retweet_count']
+	cat_vars=['place','truncated','possibly_sensitive','geo','contributors','favorited','retweeted']
 	data_vars=df.columns.values.tolist()
 	to_keep=[i for i in data_vars if i not in cat_vars]
 
-	####################################Drop nan and strings############################################
+	df = df[to_keep]
+	########
+
+
 	cols_to_remove = []
 
 	for col in df.columns:
@@ -44,13 +45,30 @@ def featureEncoding(df):
 	# keep only the columns in df that do not contain string
 	df = df[[col for col in df.columns if col not in cols_to_remove]]
 
-	df = df.dropna(axis=1)
-	df.head()
+	###########################################Feature Selection########################################
+	# Recursive Feature Elimination
+	from sklearn.feature_selection import RFE
+	from sklearn.linear_model import LogisticRegression
 
-###########################################Selected Features########################################
-	cols = ['place_nan','num_hashtags_2.0','num_hashtags_3.0','num_hashtags_4.0',
-	        'num_urls_0.0','num_mentions_1.0','num_mentions_2.0','num_mentions_3.0',
-	        'favorite_count_favorite_count0_20','retweet_count_retweet_count20_40k']
+	# create a base classifier used to evaluate a subset of attributes
+	model = LogisticRegression()
+	# create the RFE model and select 3 attributes
+	rfe = RFE(model, 15)
+	rfe = rfe.fit(df, dataOrigin.bot)
+	# summarize the selection of the attributes
+	print(rfe.support_)
+	print(rfe.ranking_)
+	sup = rfe.support_
+
+	col_temp = []
+	index = 0
+	for c in sup:
+	    if c == True:
+	        col_temp.append(df.columns[index])
+	    index += 1
+
+	###########################################Selected Features########################################
+	cols = col_temp
 	X = df[cols]
 	y = dataOrigin['bot']
 
